b354428f7fb2a346d9992e265fcac08b5a73ad30
anastasiasnzh <aastasuhan@gmail.com>
2017-12-02 01:03:09 +0200

    Exit door and win level pop up

---
+++ Assets/Scripts/DoorExit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorExit : Collectable
+{
+    protected override void OnRabitHit(HeroRabit rabit)
+    {
+        //Level.current.addCoins(1);
+        this.CollectedHide();
+        GameController.current.setCurrentLevel(0);
+        SceneManager.LoadScene("ChooseLevelScene");
+    }
+}
