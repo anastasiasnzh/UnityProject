476cea0b5129bc2508bdccb0937511bf5864fcba
anastasiasnzh <aastasuhan@gmail.com>
2017-12-08 14:37:11 +0200

    Some update

--- Assets/Scripts/DoorExit.cs
+++ Assets/Scripts/DoorExit.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class DoorExit : Collectable
+
+
 {
+
+    public GameObject winPrefab;
+
     protected override void OnRabitHit(HeroRabit rabit)
     {
         //Level.current.addCoins(1);
         this.CollectedHide();
         GameController.current.setCurrentLevel(0);
+        this.showSettings();
         SceneManager.LoadScene("ChooseLevelScene");
+    }
+
+    void showSettings()
+    {
+        //Знайти батьківський елемент
+        GameObject parent = UICamera.first.transform.parent.gameObject;
+        //Створити Prefab
+        GameObject obj = NGUITools.AddChild(parent, winPrefab);
+        //Отримати доступ до компоненту (щоб передати параметри)
+        SettingsPopUp popup = obj.GetComponent<SettingsPopUp>();
+        //...
+
+        //popup.setFruitsCounts(3, 13);//
     }
 }
